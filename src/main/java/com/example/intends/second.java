9f5440c2244d0290a7e5d739e53ae32ab5a8ac0c
HongchuanZhang <39239452+zhangtrue@users.noreply.github.com>
2020-04-28 11:31:27 +0800

    Add files via upload

---
+++ src/main/java/com/example/intends/second.java
@@ -0,0 +1,17 @@
+package com.example.intends;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2018/3/27.
+ */
+
+public class second extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+    }
+
+}
